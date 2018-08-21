bc083f8ee6f75ba9d158b30f86b8c4e9a171df2c
Alex <bozeak@gmail.com>
2018-08-21 13:43:22 +0300

    EASYOPAC-937 - Display block title and open links in new tab.

--- templates/ding-social--instagram-block-item.tpl.php
+++ templates/ding-social--instagram-block-item.tpl.php
@@ -8,19 +8,19 @@
 
 <div class="ding-social-insta-item">
     <div class="ding-social-insta-item-image">
-        <a href="<?php print $href; ?>">
+        <a href="<?php print $href; ?>" target="_blank">
             <img data-lazy="<?php print $src; ?>" alt="">
         </a>
     </div>
     <div class="ding-social-insta-item-user">
         <div class="profile">
             <div class="profile-photo">
-                <a href="https://instagram.com/<?php print $post->user->username; ?>">
+                <a href="https://instagram.com/<?php print $post->user->username; ?>" target="_blank">
                     <img src="<?php print $post->user->profile_picture; ?>" alt="">
                 </a>
             </div>
             <div class="profile-name">
-                <a href="https://instagram.com/<?php print $post->user->username; ?>">
+                <a href="https://instagram.com/<?php print $post->user->username; ?>" target="_blank">
                   <?php print $post->user->full_name; ?>
                 </a>
             </div>
